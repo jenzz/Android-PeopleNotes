9604eb1b89bf801d4c1565e3c86fd4fbd7f84c38
jenzz <github@jenzz.com>
2021-12-03 10:19:27 +0100

    Code clean up

--- app/src/main/java/com/jenzz/peoplenotes/MainActivity.kt
+++ app/src/main/java/com/jenzz/peoplenotes/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.jenzz.peoplenotes.ui.theme.AndroidPeopleNotesTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             AndroidPeopleNotesTheme {
                 Surface(color = MaterialTheme.colors.background) {
--- app/src/main/java/com/jenzz/peoplenotes/ui/theme/Theme.kt
+++ app/src/main/java/com/jenzz/peoplenotes/ui/theme/Theme.kt
@@ -5,32 +5,30 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    secondary = Teal200,
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
 )
 
 @Composable
 fun AndroidPeopleNotesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colors = if (darkTheme) {
         DarkColorPalette
@@ -42,6 +40,6 @@ fun AndroidPeopleNotesTheme(
         colors = colors,
         typography = Typography,
         shapes = Shapes,
-        content = content
+        content = content,
     )
 }
