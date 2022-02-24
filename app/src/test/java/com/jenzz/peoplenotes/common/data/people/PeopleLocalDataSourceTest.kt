bc2a54b7d6625515194e62de2025b9e6b047b85f
jenzz <github@jenzz.com>
2022-02-24 17:22:40 +0100

    Fix CoroutineTestRule & Explicitly pass dispatchers to SQLDelight mappers

--- app/src/test/java/com/jenzz/peoplenotes/common/data/people/PeopleLocalDataSourceTest.kt
+++ app/src/test/java/com/jenzz/peoplenotes/common/data/people/PeopleLocalDataSourceTest.kt
@@ -4,13 +4,12 @@ import androidx.compose.ui.graphics.Color
 import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import com.jenzz.peoplenotes.Database
-import com.jenzz.peoplenotes.common.data.TestCoroutineDispatchers
 import com.jenzz.peoplenotes.common.data.time.SystemClock
 import com.jenzz.peoplenotes.ext.random
 import com.jenzz.peoplenotes.ext.toNonEmptyString
 import com.jenzz.peoplenotes.feature.people.ui.PeopleSortBy
+import com.jenzz.peoplenotes.rules.CoroutineTestRule
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
-import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import java.util.*
@@ -22,15 +21,14 @@ class PeopleLocalDataSourceTest {
         Properties().apply { put("foreign_keys", "true") }
     ).also { Database.Schema.create(it) }
 
-    private val testScope = TestScope()
     private val sut = PeopleLocalDataSource(
         personQueries = Database(driver).personQueries,
-        dispatchers = TestCoroutineDispatchers(testScope),
+        dispatchers = CoroutineTestRule().testCoroutineDispatchers,
         clock = SystemClock(),
     )
 
     @Test
-    fun `sorts people by first name`() = testScope.runTest {
+    fun `sorts people by first name`() = runTest {
         val person1 = NewPerson(
             firstName = FirstName("John".toNonEmptyString()),
             lastName = LastName("Doe".toNonEmptyString()),
